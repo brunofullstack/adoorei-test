2f2b55d2dbb57ca1f142748e56ace5831e52ddd0
Bruno Carvalho <bruno.carvalho@hotmail.de>
2023-03-26 13:31:33 -0300

    fix: Changing app structure

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavBar />
+  <router-view />
+  <footer>
+    <FooterBar />
+  </footer>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+import FooterBar from "@/components/FooterBar.vue";
+export default {
+  components: { NavBar, FooterBar },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-Vue.config.productionTip = false
-Vue.config.devtools = true
-
-new Vue({
-  router, store,
-  render: h => h(App)
-}).$mount('#app')
+const axios = require("axios").default;
+axios.defaults.baseURL = "https://fakestoreapi.com";
+createApp(App).use(store).use(router, axios).mount("#app");
